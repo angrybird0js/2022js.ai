b594fda802c332da6966076df9822be15f5a5cdf
angrybird0js <jaehsh@naver.com>
2022-10-21 11:28:12 +0900

    retry

--- navigator.js
+++ navigator.js
@@ -1,77 +1,36 @@
-// 페이지 이동 인덱스를 제공
-// node, element 
 
-// document.getElementById(id)
-// document.getElementsByTagName(name)
-// document.createElement(name)
-// parentNode.appendChild(node)
-// element.innerHTML
-// element.style.left
-// element.setAttribute
-// element.getAttribute
-// element.addEventListener
-// window.content (en-US)
-// window.onload (en-US)
-// window.dump (en-US)
-// window.scrollTo (en-US)
+    // 교재 320p.
+
+    var idxList = ["[Home]","[Dev Class]","[Dev Note]","[Contact]"];
+    var purl = [
+            "./index.html",
+            "./class.html",
+            "./devnote.html",
+            "./contact.html",
+        ]; 
+    (window.onload = function() {
+        
 
 
-// 그외 만들것 : conjugation-json , guestboard-mongoDB, hangul-canvas , 
+    var hdr = document.getElementById('header-idx');
+        for (var i=0;i<=idxList.length;i++) {
 
 
+            const hd = document.createElement('a');
+            
+            hd.textContent = idxList[i];
+            hd.setAttribute('href' , purl[i]);
+            hdr.appendChild(hd);
 
-//블록 내부에 subPage
+            //var navList = document.createElement("a");
+            //var itemList = document.createTextNode(idxList[i]);
 
-//키 값 쌍으로 구조체형으로 만들고 주소와 목록
-// 배열 대신 Hash
+            //navList.appendChild(itemList);
+            //navList.href = purl[i];
+            //navList.text = idxList[i];
+            
 
+            //document.body.appendChild(navList);
 
-
-class subPage {
-
-    constructor(pttl , purl) {
-        this.pttl = pttl;
-        this.purl = purl;
-    }
-// get set
-
-}
-
-//prototype, get, set
-//argument
-
-//function subPage() {} 로 선언했을경우,
-//subPage.prototype = {}
-
-// style 지정 후 .css 파일 배당 
-
-// a TAG 내부에 href 속성과 리스트 사용
-function recordIndex(url, ttl) {
-    var hdr = getElementById("header");
-    var navList = document.createElement(a);
-    navList.href = url;
-
-    // 항목명 배정 navlist. = ttl;
-    //attr class 지정 css 선택자 지정
-    
-    hdr.appendChild(navList);  
-
-}
-
-//function generateIndex() {}
-//var listIndex = subPage();
-//listIndex.setIndex = { : }
-
-//listIndex.
-
-//for (var i =0 ; elements.length; i++) {}
-//recordIndex
-
-
-
-//foreach () 
-//subPage class 의 instance 생성
-
-// view css 적용될수 있게 함 
-//window.onload =function() {}
-
+        }
+    })();

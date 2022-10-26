dc2b892adbc03f6497a36eabb722338adf4e540b
angrybird0js <jaehsh@naver.com>
2022-10-26 17:32:33 +0900

    risk dice

--- cmt_ref.js
+++ cmt_ref.js
@@ -7,4 +7,38 @@
 // https://photos.google.com/share/AF1QipMppMn8mFbs8-0BVfxk8ULnYDRiwko9ZdxCV4KX4Vs9tjJ7tBZkGrSRqm0Jih3_7w?key=UXVJUkdMWEo5Y0tHNDhXUnp3YVdUdUNQVTB3a0JR
 //
 
+// DOM
+// https://ko.javascript.info/dom-navigation
 
+
+// MAP REDUCE
+// https://developer-talk.tistory.com/146
+// https://www.zerocho.com/category/JavaScript/post/5acafb05f24445001b8d796d
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+// https://stackoverflow.com/questions/14810506/map-function-for-objects-instead-of-arrays
+
+// https://stackoverflow.com/questions/55538806/javascript-factorial-function-memoization
+
+// let cache = {0: 1};
+// function factMemoize(key) {
+//     if (!cache[key]) {
+//         let maxVal = Object.keys(cache).reduce(function (a, b) {
+//             return Math.max(a, b);
+//         });
+//         console.log(maxVal);
+//         while (key >= maxVal) {
+//             cache[key] = key * factMemoize(key - 1);
+//             maxVal++;
+//         }
+//     }
+//     return cache[key];
+// }
+
+// Memoization
+// https://medium.com/@soyoung823/memoization-cache-f8b5930e3ee1
+// https://www.freecodecamp.org/news/understanding-memoize-in-javascript-51d07d19430e/
+
+
+// Promise
+// https://elvanov.com/2597
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
--- navigator.js
+++ navigator.js
@@ -1,19 +1,23 @@
 
     // 교재 320p.
 
-    const idxList = ["[Home]","[Dev Class]","[Dev Note]","[Contact]"];
+    const idxList = ["[Home]","[Dev Class]","[Dev Note]","[About]"];
     const purl = [
             "./index.html",
             "./class.html",
             "./devnote.html",
-            "./contact.html",
+            "./about.html",
         ]; 
     (window.onload = function() {
         showIndex();
-        //topMargin();
-        //한번더 나오는 문제가 있으며 이 함수를 호출하지 않고 
-        //본문내의 div의 top 을 충분히 주면 해결된다.
+
+
     })();
+
+
+
+
+    // hd.setAttribute('class' , navitem);
 
     function showIndex() {
     //dvi 영역까지 스크립트로 흡수할것
@@ -24,34 +28,90 @@
     // 2개 출력되는 현상은 스타일시트 정리 후 사라짐
     // 
     //var hdr = document.getElementById('header-idx');
+        //격자괄호(대괄호)가 HOVER 상태에서만 나타나게 할것.
+        // mouseon 에 따르는 설명창 추가
+
+        // 아이콘 추가 += 연산자로 문자열 처리
+        // dvi 내에 항목을 위치 (완료)
 
     for (var i=0;i<=idxList.length;i++) {
 
+            const dv_hdr = document.createElement('div');
+            dv_hdr.setAttribute('class' , "navitem");
+            hdr.appendChild(dv_hdr);
 
             const hd = document.createElement('a');
             
             hd.textContent = idxList[i];
             hd.setAttribute('href' , purl[i]);
-            hdr.appendChild(hd);
+            hd.setAttribute('class' , "lstitem");
 
-            //var navList = document.createElement("a");
+            dv_hdr.appendChild(hd);
+
+
+            
             //var itemList = document.createTextNode(idxList[i]);
-
+            // TAG 를 생성하지 못함
+            //var navList = document.createElement("a");
             //navList.appendChild(itemList);
             //navList.href = purl[i];
             //navList.text = idxList[i];
-            
+            //itemList.appendChild(navList);
 
             //document.body.appendChild(navList);
+            // 대상에 제대로 타겟을 잡지 못함
+            }
 
-        }
 
 
-    }
 
-    function topMargin() {
-        const hddr = document.createElement('div');
-        hddr.setAttribute('id' , "hdr-idx");
-        document.body.appendChild(hddr);
+            const selItem = document.getElementsByClassName('lstitem');
+            
+            selItem.addEventListner("mouseover", () => {
+                console.log("정상진행");
+                hd.textContent = `"[" + idxList + "]"`;
+            });
+
+
+
+        // const testIdx = [ "item1" , "item2" , "item3" ];
+        // const prtIdx = testIdx.map( function(event) { `${testIdx}` } );
+        
+        
+        // for (var j=0; j<=prtIdx.length; j++) {document.write(`<p>$(prtIdx[j])</p>`);}
+        //while(prtIdx) {console.log(prtIdx);}
+        
+        // document.addEventListener('DOMContentLoaded' , () => {
+        //     const navbar = document.querySelector('a');
+        //     navbar.addEventListner("mouseon", (event) => {
+        //         navbar
+        //     });
+
+        // });
+        // mouse pointer 위치할때마다 대괄호 추가
+        // hd는 전체항목이므로 개별 아이템에 접근해야 한다.
+        
+
+
+        // if (hd.hasChildNodes()) {
+        //     var pglink = hd.childNodes;
+        //     for (var i = 0; i < pglink.length; i++) {
+        //         //
+        //         pglink[i].addEventListner("mouseon", function(event) { 
+                    
+        //             pglink[i]=("["+${pglink[i]}+"]"); 
+        //             hdr.appendChild(hd);
+        //         });
+        //         // cancat , join(array)
+
+        //     }
+        // };
+
     };
-     //2개 출력된다.
+
+    // function topMargin() {
+    //     const hddr = document.createElement('div');
+    //     hddr.setAttribute('id' , "hdr-idx");
+    //     document.body.appendChild(hddr);
+    // };
+     //2개 출력된다. 호출부 또한 비활성화함.
